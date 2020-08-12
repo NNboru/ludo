7fef4ccb943a08938df5686c2abf6bdc38bf4f93
nn <rohan.rawat.9897@gmail.com>
2020-08-12 12:56:18 +0530

    1st

---
+++ ludo.js
@@ -0,0 +1,486 @@
+rpos = [ [11.3,69.2],[11.3,81.6],[23.4,81.6],[23.4,69.2] ]
+bpos = [ [11.3,9.2],[23.4,21.6],[11.3,21.6],[23.4,9.2] ]
+ypos = [ [71.3,9.2],[83.4,21.6],[71.3,21.6],[83.4,9.2] ]
+gpos = [ [71.3,69.2],[83.4,81.6],[71.3,81.6],[83.4,69.2] ]
+
+pathred=[
+    [40.8,85.2],[40.8,78.6],[40.8,72],[40.8,65.4],[40.8,58.7],[34.2,52.1],[27.6,52.1],
+    [21,52.1],[14.4,52.1],[7.8,52.1],[1.1,52],[1.1,45.4],[1.1,38.8],[7.8,38.8],
+    [14.4,38.8],[21,38.8],[27.6,38.8],[34.2,38.8],[40.8,32],[40.8,25.4],[40.8,18.6],
+    [40.8,12.2],[40.8,5.6],[40.8,-1.2],[47.4,-1.2],[54.2,-0.8],[54.2,6],[54.2,12.6],
+    [54.2,19],[54.2,25.8],[54.2,32.4],[60.8,39.2],[67.4,39.2],[74,39.2],[80.6,39.2],
+    [87.2,39.2],[93.9,39.2],[93.9,45.8],[93.9,52.4],[87.2,52.5],[80.6,52.5],[74,52.5],
+    [67.4,52.5],[60.8,52.5],[54.2,59.1],[54.2,65.8],[54.2,72.4],[54.2,79],[54.2,85.6],
+    [54.2,92.3],[47.6,92.3],
+    [47.6,85.3],[47.6,78.7],[47.6,72.1],[47.6,65.5],[47.6,58.9],[47.4,51]
+]
+pathblue=[
+    [7.8,38.8],[14.4,38.8],[21,38.8],[27.6,38.8],[34.2,38.8],[40.8,32],[40.8,25.4],
+    [40.8,18.6],[40.8,12.2],[40.8,5.6],[40.8,-1.2],[47.4,-1.2],[54.2,-0.8],[54.2,6],
+    [54.2,12.6],[54.2,19],[54.2,25.8],[54.2,32.4],[60.8,39.2],[67.4,39.2],[74,39.2],
+    [80.6,39.2],[87.2,39.2],[93.9,39.2],[93.9,45.8],[93.9,52.4],[87.2,52.5],
+    [80.6,52.5],[74,52.5],[67.4,52.5],[60.8,52.5],[54.2,59.1],[54.2,65.8],[54.2,72.4],
+    [54.2,79],[54.2,85.6],[54.2,92.3],[47.6,92.3],[40.8,91.9],[40.8,85.2],[40.8,78.6],
+    [40.8,72],[40.8,65.4],[40.8,58.7],[34.2,52.1],[27.6,52.1],[21,52.1],[14.4,52.1],
+    [7.8,52.1],[1.1,52],[1.1,45.4],
+    [7.6,45.4],[14.2,45.4],[20.8,45.4],[27.4,45.4],[34,45.4],[40.8,45]
+]
+pathyellow=[
+    [54.2,6],[54.2,12.6],[54.2,19],[54.2,25.8],[54.2,32.4],[60.8,39.2],[67.4,39.2],
+    [74,39.2],[80.6,39.2],[87.2,39.2],[93.9,39.2],[93.9,45.8],[93.9,52.4],[87.2,52.5],
+    [80.6,52.5],[74,52.5],[67.4,52.5],[60.8,52.5],[54.2,59.1],[54.2,65.8],[54.2,72.4],
+    [54.2,79],[54.2,85.6],[54.2,92.3],[47.6,92.3],[40.8,91.9],[40.8,85.2],[40.8,78.6],
+    [40.8,72],[40.8,65.4],[40.8,58.7],[34.2,52.1],[27.6,52.1],[21,52.1],[14.4,52.1],
+    [7.8,52.1],[1.1,52],[1.1,45.4],[1.1,38.8],[7.8,38.8],[14.4,38.8],[21,38.8],
+    [27.6,38.8],[34.2,38.8],[40.8,32],[40.8,25.4],[40.8,18.6],[40.8,12.2],
+    [40.8,5.6],[40.8,-1.2],[47.4,-1.2],
+    [47.6,6],[47.6,12.6],[47.6,19],[47.6,25.8],[47.6,32.4],[47.5,40]
+]
+pathgreen=[
+    [87.2,52.5],[80.6,52.5],[74,52.5],[67.4,52.5],[60.8,52.5],[54.2,59.1],[54.2,65.8],
+    [54.2,72.4],[54.2,79],[54.2,85.6],[54.2,92.3],[47.6,92.3],[40.8,91.9],[40.8,85.2],
+    [40.8,78.6],[40.8,72],[40.8,65.4],[40.8,58.7],[34.2,52.1],[27.6,52.1],[21,52.1],
+    [14.4,52.1],[7.8,52.1],[1.1,52],[1.1,45.4],[1.1,38.8],[7.8,38.8],[14.4,38.8],
+    [21,38.8],[27.6,38.8],[34.2,38.8],[40.8,32],[40.8,25.4],[40.8,18.6],[40.8,12.2],
+    [40.8,5.6],[40.8,-1.2],[47.4,-1.2],[54.2,-0.8],[54.2,6],[54.2,12.6],[54.2,19],
+    [54.2,25.8],[54.2,32.4],[60.8,39.2],[67.4,39.2],[74,39.2],[80.6,39.2],
+    [87.2,39.2],[93.9,39.2],[93.9,45.8],
+    [87.2,46],[80.6,46],[74,46],[67.4,46],[60.8,46],[54.5,46]
+]
+
+track=[ 
+        [40.8, 92.7], [40.8, 86],   [40.8,79.4],  [40.8,72.8],  [40.8,66.2],  [40.8,59.5],
+        [34.2, 52.9], [27.6, 52.9], [21.0, 52.9], [14.4, 52.9], [7.8, 52.9], [1.1, 52.8],
+        [1.1, 46.2] , 
+        [1.1, 39.6] , [7.8, 39.6], [14.4, 39.6], [21.0, 39.6], [27.6, 39.6], [34.2, 39.6],
+        [40.8, 32.8], [40.8, 26.2], [40.8, 19.4], [40.8, 13], [40.8, 6.4], [40.8, -.4],
+        [47.4, -.4],
+        [54.2, 0.0], [54.2, 6.8], [54.2, 13.4], [54.2, 19.8], [54.2, 26.6], [54.2, 33.2], 
+        [60.8, 40.0], [67.4, 40.0], [74.0, 40.0], [80.6, 40.0], [87.2, 40.0], [93.9, 40.0], 
+        [93.9, 46.6], 
+        [93.9, 53.2], [87.2, 53.3], [80.6, 53.3], [74.0, 53.3], [67.4, 53.3], [60.8, 53.3], 
+        [54.2, 59.9], [54.2, 66.6], [54.2, 73.2], [54.2, 79.8], [54.2, 86.4], [54.2, 93.1], 
+        [47.6, 93.1],
+
+        [47.6,85.3],[47.6,78.7],[47.6,72.1],[47.6,65.5],[47.6,58.9],
+        [7.6,45.4],[14.2,45.4],[20.8,45.4],[27.4,45.4],[34,45.4],
+        [47.6,6],[47.6,12.6],[47.6,19],[47.6,25.8],[47.6,32.4],
+        [87.2,46],[80.6,46],[74,46],[67.4,46],[60.8,46]
+
+]
+
+let xypos = [ [0,0],[1,0],[0,1],[0,-1],[-1,0],[2,0] ];
+
+objstore = [
+    {path:pathblue  , goti:Array.from(document.getElementsByClassName('blue' )),
+          color:'blue', surfL:'5.6', surfT:'5.6', diceL:'16', diceT:'17' },
+    {path:pathyellow, goti:Array.from(document.getElementsByClassName('yellow')),
+        color:'yellow', surfL:'65.6',surfT:'5.6', diceL:'77', diceT:'17' },
+    {path:pathgreen , goti:Array.from(document.getElementsByClassName('green')), 
+        color:'green',  surfL:'65.6',surfT:'65.6',diceL:'77', diceT:'77' },
+    {path:pathred   , goti:Array.from(document.getElementsByClassName('red')  ), 
+        color:'red'  ,  surfL:'5.6',  surfT:'65.6',diceL:'16', diceT:'77' },
+];
+
+function rand(min,max){
+    return Math.trunc(min + Math.random()*(max-min+1));
+}
+
+function timer(t=200){
+    return new Promise((r)=>setTimeout(() => {
+        r();
+    }, t))
+}
+
+async function dto(){
+    for(let x=0;x<=3600;x++){
+        d1.style.transform=`rotateY(${x}deg) rotateX(${2*x}deg) translateZ(40px)`;
+        d2.style.transform=`rotateY(${x}deg) rotateX(${2*x}deg) translateY(40px)  rotateX(-90deg)`;
+        d3.style.transform=`rotateY(${x}deg) rotateX(${2*x}deg) translateY(-40px) rotateX(90deg)`;
+        d4.style.transform=`rotateY(${x}deg) rotateX(${2*x}deg) translateX(40px)  rotateY(90deg)`;
+        d5.style.transform=`rotateY(${x}deg) rotateX(${2*x}deg) translateX(-40px) rotateY(-90deg)`;
+        d6.style.transform=`rotateY(${x}deg) rotateX(${2*x}deg) translateZ(-40px) rotateX(180deg)`;
+        await timer(10);
+    }
+}
+
+
+function Lpos(x){
+    return `calc(var(--left) + ${x}*var(--pre))`;
+}
+function Tpos(x){
+    return `calc(var(--top) + ${x}*var(--pre))`;
+}
+
+
+let sides = document.querySelectorAll('#dice>div')
+function roll(){
+    dice.onclick=null;
+    let x = rand(20,bdy.offsetWidth-100),y= rand(20,bdy.offsetHeight-100);
+    let time = (.3+Math.random()/2);
+    if(!audiroll.ended){
+        audiroll.pause();
+        audiroll.currentTime=0;
+    }
+    audiroll.playbackRate=(0.66/time + .2);
+    audiroll.play();
+    time=time+'s';
+    for(i = 0; i < sides.length; i++){
+        sides[i].style.transitionDuration=time;
+    }
+    dice.style.transitionDuration=time;
+    dice.style.left=x+'px';
+    dice.style.top=y+'px';
+    dice.style.animationDuration=time;
+    dice.style.animationName='ani-scale';
+    let ind = Math.min(5,rand(0,5+six[chance]));
+    if(ind==5) six[chance]=-.4;
+    else six[chance]+=.5;
+    if(cheat!=-1){
+        ind=cheat;
+        // cheat=-1;
+    }
+    [x,y] = xypos[ind];
+    // console.log(ind+1);
+    x=x*90 + rand(-2,2)*360 - 8+rand(0,1)*16;
+    y=y*90 + rand(-2,2)*360 - 8+rand(0,1)*16;
+    d1.style.transform=`rotateY(${y}deg) rotateX(${x}deg) translateZ(20px)`;
+    d2.style.transform=`rotateY(${y}deg) rotateX(${x}deg) translateY(20px)  rotateX(-90deg)`;
+    d3.style.transform=`rotateY(${y}deg) rotateX(${x}deg) translateY(-20px) rotateX(90deg)`;
+    d4.style.transform=`rotateY(${y}deg) rotateX(${x}deg) translateX(20px)  rotateY(90deg)`;
+    d5.style.transform=`rotateY(${y}deg) rotateX(${x}deg) translateX(-20px) rotateY(-90deg)`;
+    d6.style.transform=`rotateY(${y}deg) rotateX(${x}deg) translateZ(-20px) rotateX(180deg)`;
+    dice.style.cursor='default';
+    num = ind+1;
+}
+
+function onrolled(){
+    dice.style.animationName='none';
+    surface.style.transform='scale(0)';
+    dice.style.transitionDuration='.3s';
+    torise=[];
+    let x,y;
+    for(let g=0; g<obj[chance]['goti'].length ; ++g){
+        let goti=obj[chance]['goti'][g];
+        if(num>56-goti.dataset.ind || (goti.dataset.ind==-1 && num!=6)) continue;
+        torise.push(goti);
+        [x,y] = [goti.style.left,goti.style.top];
+    }
+    if(torise.length==0){
+        chance=(chance+1)%obj.length;
+        nextplayer();
+    }
+    else if(torise.every(v=>v.style.left==x && v.style.top==y)){
+        movegoti({target:torise[0].firstElementChild});
+    }
+    else{
+        //show your goti - badi goti
+        for(let goti of torise){
+            goti.classList.add('largegoti');
+            goti.onclick=movegoti;
+        }
+    }
+}
+
+allgotis = document.getElementsByClassName('goti');
+function gotisat(x,y){
+    let gotis = [];
+    for(let goti of allgotis){
+        if(goti.style.left==x && goti.style.top==y)
+            gotis.push(goti);
+    }
+    return gotis;
+}
+
+function arrange(gotis){
+    let len=gotis.length;
+    if(len==0) return;
+    if(len==1){
+        gotis[0].style.transform='';
+        return;
+    };
+    let ind=0, times=Math.ceil(len/3)-1, sc=[.8,.7,.6,.6,.6,.6][times], bias=4*(1-sc), tp=times?((2+times*1.6)/times):0,y=1.8-bias ;
+    for(let i=0;i<=times;i++){
+        let cnt=Math.min(3,len-ind);
+        if(cnt==1){
+            gotis[ind].style.transform=`scale(${sc}) translate(calc(${bias}*var(--pre)),calc(${y}*var(--pre)))`;
+        }
+        else if(cnt==2){
+            gotis[ind].style.transform=`scale(${sc}) translate(calc(${bias+1.8+times/2}*var(--pre)),calc(${y}*var(--pre)))`;
+            gotis[ind+1].style.transform=`scale(${sc}) translate(calc(${bias-1.8-times/2}*var(--pre)),calc(${y}*var(--pre)))`;
+        }
+        else{
+            gotis[ind].style.transform=`scale(${sc}) translate(calc(${bias+2+times/2}*var(--pre)),calc(${y}*var(--pre)))`;
+            gotis[ind+1].style.transform=`scale(${sc}) translate(calc(${bias}*var(--pre)),calc(${y}*var(--pre)))`;
+            gotis[ind+2].style.transform=`scale(${sc}) translate(calc(${bias-2-times/2}*var(--pre)),calc(${y}*var(--pre)))`;
+
+        }
+        y+=tp;
+        ind+=3;
+    }
+}
+
+async function movegoti({target}){
+    //back to normal goti
+    for(let goti of torise){
+        goti.classList.remove('largegoti');
+        goti.onclick=null;
+    }
+    //moving..
+    let goti = target.parentElement;
+    goti.style.transform='';
+    goti.style.transitionDuration='.24s';
+    let old = [goti.style.left,goti.style.top];
+    if(goti.dataset.ind==-1) num=1;
+
+    let ind= +goti.dataset.ind,x,y;
+    for(let i=0;i<num;i++){
+        ind++;
+        [x,y] = obj[chance]['path'][ind];
+        x=Lpos(x),y=Tpos(y);
+        goti.style.left=x;
+        goti.style.top =y;
+        audimove.play()
+        await timer(241);
+    }
+    goti.dataset.ind = ind;
+    if(ind!=0 && ind<51 && safepos.includes(ind))
+        audisafe.play();
+    //arrange old place
+    arrange(gotisat(old[0],old[1]));
+    //arrange new place
+    [gotis,flag] = await cutgoti(gotisat(x,y),ind);
+    arrange(gotis);
+    //if at home
+    if(ind==56){
+        obj[chance]['goti'].splice(obj[chance]['goti'].indexOf(goti),1);
+        if(obj[chance]['goti'].length==0){
+            let player = obj.splice(chance,1)[0];
+            if(chance==obj.length) chance=0;
+            numofplayer--;
+            let im = medals.pop();
+            im.style.left=Lpos(player.surfL);
+            im.style.top =Tpos(player.surfT);
+            im.style.transform = 'scale(1) translate(-1%, -5%)';
+            audiwin.play();
+
+            if(numofplayer==1){
+                //gameover
+                await timer(200);
+                im = medals[0];
+                im.style.left=Lpos(obj[0].surfL);
+                im.style.top =Tpos(obj[0].surfT);
+                im.style.transform = 'scale(1) translate(-1%, -5%)';
+                await timer(2000);
+                dlgre.style.transform='scale(1)';
+                return;
+            }
+            await timer(400);
+        }
+        flag=1;
+    }
+    //see next chance
+    if(!flag && ind!=0 && num!=6){
+        chance=(chance+1)%obj.length;
+    }
+    nextplayer();
+}
+
+let safepos = [0,8,13,21,26,34,39,47,51,52,53,54,55,56];
+async function cutgoti(gotis,ind){
+    if(safepos.includes(ind))
+        return [gotis,false];
+    let len=gotis.length, c=obj[chance].color, cut=0,tmp=0,time,g;
+    for(let i=len-1;i>=0;i--){
+        if(!gotis[i].classList.contains(c)){
+            cut=1;
+            g=gotis.splice(i,1)[0], time=(.2+(+g.dataset.ind)*.05);
+            g.style.transitionDuration=time+'s';
+            if(!audicut.ended){
+                audicut.pause();
+                audicut.currentTime=0;
+            }
+            g.style.left=Lpos(g.dataset.startl);
+            g.style.top =Tpos(g.dataset.startt);
+            g.dataset.ind=-1;
+            if(time>tmp) tmp=time;
+        }
+    }
+    if(cut){
+        audicut.playbackRate=1.172/time;
+        audicut.play();
+        await timer(tmp);
+    }
+    return [gotis,cut];
+}
+
+function nextplayer(){
+    surface.style.left=obj[chance].surfL+'%';
+    surface.style.top =obj[chance].surfT+'%';
+    surface.style.transform='scale(1)';
+    dice.style.left=Lpos(obj[chance].diceL);
+    dice.style.top =Tpos(obj[chance].diceT);
+    dice.style.cursor='pointer';
+    dice.onclick=roll;
+    dice.onanimationend=onrolled;
+    bdy.style.backgroundColor=obj[chance].color;
+}
+
+//events
+dlgstart.onclick=e=>{
+    if(e.target.classList.contains('select')){
+        if(e.target.parentElement.classList.contains('noneed')){
+            e.target.parentElement.classList.remove('noneed');
+            numofplayer++;
+        }
+        else if(numofplayer>2){
+            e.target.parentElement.classList.add('noneed');
+            numofplayer--;
+        }
+    }
+}
+butstart.onclick=e=>{
+    dlgstart.style.transform='scale(0)';
+    dice.style.transform='none';
+    obj=[];
+    chance=rand(0,numofplayer-1);
+    //back to normal goti
+    for(let goti of torise){
+        goti.classList.remove('largegoti');
+        goti.onclick=null;
+    }
+    //blue
+    if(!pl1.parentElement.classList.contains('noneed')){
+        name1.innerHTML=pl1.value;
+        let tmp = Object.assign({},objstore[0]);
+        tmp['goti']=Array.from(tmp['goti']);
+        for(let g of tmp['goti']){
+            g.style.left=Lpos(g.dataset.startl);
+            g.style.top =Tpos(g.dataset.startt);
+            g.dataset.ind='-1';
+        }
+        obj.push(tmp);
+    }
+    else{
+        name1.innerHTML='';
+        for(let g of objstore[0].goti){
+            g.style.left='-100vw';
+        }
+    }
+    //yellow
+    if(!pl4.parentElement.classList.contains('noneed')){
+        name4.innerHTML=pl4.value;
+        let tmp = Object.assign({},objstore[1]);
+        tmp['goti']=Array.from(tmp['goti']);
+        for(let g of tmp['goti']){
+            g.style.left=Lpos(g.dataset.startl);
+            g.style.top =Tpos(g.dataset.startt);
+            g.dataset.ind='-1';
+        }
+        obj.push(tmp);
+    }
+    else{
+        name4.innerHTML='';
+        for(let g of objstore[1].goti){
+            g.style.left='-100vw';
+        }
+    }
+    //green
+    if(!pl3.parentElement.classList.contains('noneed')){
+        name3.innerHTML=pl3.value;
+        let tmp = Object.assign({},objstore[2]);
+        tmp['goti']=Array.from(tmp['goti']);
+        for(let g of tmp['goti']){
+            g.style.left=Lpos(g.dataset.startl);
+            g.style.top =Tpos(g.dataset.startt);
+            g.dataset.ind='-1';
+        }
+        obj.push(tmp);
+    }
+    else{
+        name3.innerHTML='';
+        for(let g of objstore[2].goti){
+            g.style.left='-100vw';
+        }
+    }
+    //red
+    if(!pl2.parentElement.classList.contains('noneed')){
+        name2.innerHTML=pl2.value;
+        let tmp = Object.assign({},objstore[3]);
+        tmp['goti']=Array.from(tmp['goti']);
+        for(let g of tmp['goti']){
+            g.style.left=Lpos(g.dataset.startl);
+            g.style.top =Tpos(g.dataset.startt);
+            g.dataset.ind='-1';
+        }
+        obj.push(tmp);
+    }
+    else{
+        name2.innerHTML='';
+        for(let g of objstore[3].goti){
+            g.style.left='-100vw';
+        }
+    }
+    
+    medals = [win4, win3, win2, win1];
+    for(let im of medals){
+        im.style.transform='scale(0)';
+    }
+    nextplayer();
+}
+butre1.onclick = butre2.onclick=e=>{
+    dlgre.style.transform='scale(0)';
+    dlgpop.style.transform='scale(0)';
+    dlgstart.style.transform='scale(1)';
+    numofplayer=4-document.getElementsByClassName('noneed').length;
+    dice.style.top='10vh';
+    dice.style.top='80vw';
+}
+butcancel.onclick=e=>{
+    dlgpop.style.transform='scale(0)';
+}
+iconre.onclick=()=>{
+    dlgpop.style.transform='scale(1)';
+}
+divfull.onclick=()=>{
+    bdy.requestFullscreen();
+}
+bdy.onfullscreenchange=()=>{
+    if(document.fullscreen)
+        iconre.style.display=divfull.style.display='none';
+    else
+        iconre.style.display=divfull.style.display='initial';
+}
+bdy.onerror=e=>console.dir(e);
+
+//init
+{
+
+d1.style.transform=`rotateY(180deg) rotateX(8deg) translateZ(20px)`;
+d2.style.transform=`rotateY(180deg) rotateX(8deg) translateY(20px)  rotateX(-90deg)`;
+d3.style.transform=`rotateY(180deg) rotateX(8deg) translateY(-20px) rotateX(90deg)`;
+d4.style.transform=`rotateY(180deg) rotateX(8deg) translateX(20px)  rotateY(90deg)`;
+d5.style.transform=`rotateY(180deg) rotateX(8deg) translateX(-20px) rotateY(-90deg)`;
+d6.style.transform=`rotateY(180deg) rotateX(8deg) translateZ(-20px) rotateX(180deg)`;
+for(let g of allgotis){
+    g.style.left=Lpos(g.dataset.startl);
+    g.style.top =Tpos(g.dataset.startt);
+}
+
+var chance, num=0, torise=[], six=[-.2,-.2,-.2,-.2], numofplayer=4, obj, cheat=-1, medals;
+var audiroll=new Audio('files/roll.wav');
+var audimove=new Audio('files/tuii.mp3');
+var audisafe=new Audio('files/safe.mp3');
+var audicut=new Audio('files/tiyau.wav');
+var audiwin=new Audio('files/yippe.mp3');
+
+audimove.volume=.2;
+audimove.playbackRate=1.5;
+audisafe.playbackRate=1.5;
+
+}
+
+
+
+
+
